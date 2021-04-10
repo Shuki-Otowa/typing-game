cd0bf6d190eda32cca6025e05681fcf9d84790bb
Shuki Otowa <shuki.otowa@gmail.com>
2021-04-10 11:22:57 +0900

    nitial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="gameApp" class="container">
+    <div class="title">
+      <h1>The Typing Game</h1>
+    </div>
+    <button v-if="startFlg != true" class="startButton" @click="gameStart">
+      Game Start
+    </button>
+    <div v-if="startFlg" class="question-wrapper">
+      <div class="question">{{ currentQuestion }}</div>
+      <div v-if="currentQuestionCounts == questionCounts" class="message">Clear!</div>
+      <input v-model="typeBox" type="text" class="form" />
+      <div class="count">{{ currentQuestionCounts }} /{{ questionCounts }}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "gameApp",
+  data() {
+    return {
+      startFlg: "",
+      currentQuestion: "",
+      questions: [
+        "Hello World",
+        "console.log",
+        "npm run serve",
+        "git add -A",
+        "export default",
+      ],
+      typeBox: "",
+      currentQuestionCounts: 0,
+      questionCounts: 0,
+    };
+  },
+
+  methods: {
+    gameStart: function () {
+      this.startFlg = true;
+    },
+  },
+  mounted: function () {
+    this.currentQuestion = this.questions[0];
+    this.questionCounts = this.questions.length;
+  },
+  watch: {
+    typeBox: function (e) {
+      if (e === this.currentQuestion) {
+        this.questions.splice(0, 1);
+        this.currentQuestion = this.questions[0];
+        this.typeBox = "";
+        this.currentQuestionCounts = this.currentQuestionCounts + 1
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Skia-Regular_Black", "ヒラギノ角ゴシック", "Hiragino Sans",
+    "ヒラギノ丸ゴ ProN", "Hiragino Maru Gothic ProN", "Osaka", YuGothic,
+    "Yu Gothic", "Yu Gothic UI", "メイリオ", Meiryo, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 32px;
 }
+.container {
+  background-color: #7ba889;
+  width: 100vw;
+  height: 100vh;
+}
+
+.startButton {
+  margin-bottom: 20px;
+  padding: 10px 20px;
+}
+
+h1 {
+  font-size: 60px;
+  padding: 50px 0;
+}
+.question {
+  padding: 20px 0;
+}
+.message {
+  padding: 20px 0;
+}
+.form {
+  outline: none;
+  border: none;
+  width: 500px;
+  padding: 10px 0;
+}
+.count {
+  margin-top: 20px;
+}
+
 </style>
